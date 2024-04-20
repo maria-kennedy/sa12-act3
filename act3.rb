fb59f2bbafd76f8d9b94141b64f501cc28645619
Maria Kennedy <mariakingsbury1@gmail.com>
2024-04-19 22:01:34 -0500

    p3 complete

--- act3.rb
+++ act3.rb
@@ -35,8 +35,37 @@ def prob2
     # get request to find the exchange rate
     response = HTTParty.get("http://api.exchangeratesapi.io/v1/latest?access_key=#{my_api_key}&symbols=#{sc},#{tc}")
     # puts response
-    
+
     # calculate the exchange
     response['rates'][tc] * amt
 end
-puts prob2
+
+
+# Problem 3
+def prob3
+    # I choose to show upcoming shows for Wicked at the Orpheum
+    my_api_key = 'fNYc0n42bYiXBQohYNlFArWGfPCUhPZo'
+    venue = 'The Orpheum Theatre Memphis'
+    ven_id = 'KovZpZAF6etA'
+    after = "2024-04-01T00:30:00Z"
+
+    # used the venue search to find the venue id, commented out after finding venue id
+    # response = HTTParty.get("https://app.ticketmaster.com/discovery/v2/venues.json?apikey=#{my_api_key}&stateCode=TN&keyword=Orpheum")
+    # puts response
+
+    response = HTTParty.get("https://app.ticketmaster.com/discovery/v2/events.json?apikey=#{my_api_key}&venueId=#{ven_id}&keyword=wicked&startDateTime=#{after}")
+
+    # display info: name, venue, date, and time
+    events = response['_embedded']['events']
+    events.map do |event|
+        puts "Event Name: #{event['name']}"
+        puts "Venue: #{event['_embedded']['venues'][0]['name']}"
+        puts "Date: #{event['dates']['start']['localDate']}"
+        puts "Time: #{event['dates']['start']['localTime']}"
+        puts ""
+    end
+end
+
+# puts prob1
+# puts prob2
+puts prob3
